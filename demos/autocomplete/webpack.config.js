54b5a25b469423ed38551ecd7246e93915d8607e
Peter Halloran <mkwenhua101@gmail.com>
2018-01-30 21:02:06 -0600

    refactor: beautify webpack config

--- demos/autocomplete/webpack.config.js
+++ demos/autocomplete/webpack.config.js
@@ -21,7 +21,7 @@ const StylesLoader = {
             require('postcss-flexbugs-fixes'),
             autoprefixer({
               browsers: [
-                '>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9',
+                '>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'
               ],
               flexbox: 'no-2009'
             })
@@ -33,9 +33,7 @@ const StylesLoader = {
 }
 
 module.exports = {
-  entry: [
-    './main.js'
-  ],
+  entry: ['./main.js'],
   module: {
     rules: [
       {
@@ -43,16 +41,18 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader']
       },
-      StylesLoader,
+      StylesLoader
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: [
+      '*', '.js', '.jsx'
+    ],
     alias: {
-      autocomplete: path.resolve( __dirname, 'autocomplete'),
-      component:  path.resolve( __dirname, 'components'),
-      container:  path.resolve( __dirname, 'containers'),
-      stylesheet:  path.resolve( __dirname, 'styles')
+      autocomplete: path.resolve(__dirname, 'autocomplete'),
+      component: path.resolve(__dirname, 'components'),
+      container: path.resolve(__dirname, 'containers'),
+      stylesheet: path.resolve(__dirname, 'styles')
     }
   },
   output: {
@@ -60,7 +60,5 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [
-    new ExtractTextPlugin({filename: "styles.css"})
-  ]
+  plugins: [new ExtractTextPlugin({filename: "styles.css"})]
 };
